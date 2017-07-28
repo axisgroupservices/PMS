34fa1c5505623667ecc00ba0552f3bab0eecef4a
Deepak <deepak.pokhrel@hotmail.com>
2017-07-27 23:16:22 -0500

    Payout API completed

--- README.md
+++ README.md
@@ -5,3 +5,26 @@
 5. run chmod +x CreateRemoteRepository.sh
 6. ./CreateRemoteRepository.sh
 7. git remote -v : check if the remote repository is set.
+
+
+//Git Hot fix policies.
+1. Checkout to new branch call hot_fix_id_number
+2. Commit hot_fix without push.
+3. Checkout to develop branch and use this command.
+git merge hot_fix
+4. Push develop.
+
+Check if two branch is in sync: git diff develop hot_fix;
+
+
+//GetLogin Token
+curl -v https://api.sandbox.paypal.com/v1/oauth2/token \
+   -H "Accept: application/json" \
+   -H "Accept-Language: en_US" \
+   -u "AepMzCmhcr1xPK_QqAHL9l0pICYQtmFh4VdKeD9IWo7dDFv9Qiy-cMIup14W2hFmX49B_QND2xMLqyoT
+:EBJmps4QAiLqUDhg-GpJaLIHDJ47LQSBD9cLHThSVdfrorHvvPDU0GltQRiB12lgSUphPnVMrOOMWQT2" \
+   -d "grant_type=client_credentials"
+   
+   
+   
+   
--- src/main/java/org/axisgroup/base/controller/OrderConfirmationAndPaymentController.java
+++ src/main/java/org/axisgroup/base/controller/OrderConfirmationAndPaymentController.java
@@ -2,8 +2,11 @@ package org.axisgroup.base.controller;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.axisgroup.client.response.GetContractInfoBySpotIdResponse;
@@ -18,6 +21,7 @@ import org.roboadplacer.request.GetContractsInfoBySpotId;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -31,6 +35,11 @@ public class OrderConfirmationAndPaymentController {
 	@RequestMapping("/payouts")
 	public String orderConfirmationAndPayouts(@RequestParam("spotid") String spotId) {
 		String paymentStatus = null;
+		 byte[] decoded = Base64.decodeBase64(spotId);      
+	     spotId=new String(decoded);
+	     
+	     logger.info("decrypted spotID" +spotId);
+
 		try {
 			logger.info("Inside orderConfirmationAndPayouts");
 			logger.info("SpotID from frontend " + spotId);
@@ -49,12 +58,17 @@ public class OrderConfirmationAndPaymentController {
 			
 			PrettyPrinterJson.printObject(getContractInfoBySpotIdResponse);
 
+			
 			paymentStatus = splitPayments(spotId, getContractInfoBySpotIdResponse);
+			
+			if(StringUtils.isBlank(paymentStatus)){
+				paymentStatus="Paypal Info of cable operator is null. No balance transfered. Please, check Pyadvertising for manual transfer";
+			}
 
 		}
 		catch (Exception e) {
 
-			logger.debug(e);
+			logger.debug(e.getStackTrace());
 		}
 		
 		logger.info("The status of payouts api is " +paymentStatus);
@@ -64,85 +78,93 @@ public class OrderConfirmationAndPaymentController {
 	private String splitPayments(String spotId, GetContractInfoBySpotIdResponse getContractInfoBySpotIdResponse) {
 		String paymentStatus = null;
 		if (getContractInfoBySpotIdResponse != null && getContractInfoBySpotIdResponse.getContract() != null) {
-			if (getContractInfoBySpotIdResponse.getContract().isPayPalExists()==false) {
+			
+			if(!getContractInfoBySpotIdResponse.getContract().isSpotPaid()){
+			if (getContractInfoBySpotIdResponse.getContract().isPayPalExists()) {
 				try {
 					String cableOperatorPaypalInfo = getContractInfoBySpotIdResponse.getContract()
 							.getPayPalAccountInfo();
 					logger.info("Paypal Info of cable operator is "+cableOperatorPaypalInfo);
 
+					
 					String pyAdvertisingPaypalInfo = ConfigurationHandler
 							.getValueToConfigurationKey(PYADVERTISING_PAYPAL_ACCOUNT, CONFIG_LOCATION);
+					
+					if(StringUtils.isNotBlank(cableOperatorPaypalInfo) && StringUtils.isNotBlank(pyAdvertisingPaypalInfo)){
+						
+						List<String> splitsPaymentsStakeHolders = generateSplitAmountToStakeHolders(getContractInfoBySpotIdResponse);
 
-					List<String> splitsPaymentsStakeHolders = generateSplitAmountToStakeHolders(getContractInfoBySpotIdResponse);
+						if (splitsPaymentsStakeHolders != null && splitsPaymentsStakeHolders.size() > 1) {
+							logger.info("Pyadvertising paypalinfo from config " + pyAdvertisingPaypalInfo);
 
-					if (splitsPaymentsStakeHolders != null && splitsPaymentsStakeHolders.size() > 1) {
-						logger.info("Pyadvertising paypalinfo from config " + pyAdvertisingPaypalInfo);
+							logger.info(
+									"Amount that is transfered to cable operator is " + splitsPaymentsStakeHolders.get(0));
+							logger.info(
+									"Amount that is transfered to pyAdvertising is " + splitsPaymentsStakeHolders.get(1));
 
-						logger.info(
-								"Amount that is transfered to cable operator is " + splitsPaymentsStakeHolders.get(0));
-						logger.info(
-								"Amount that is transfered to pyAdvertising is " + splitsPaymentsStakeHolders.get(1));
+							String amountToCableOperator = splitsPaymentsStakeHolders.get(0);
+							String amountToPyAdvertising = splitsPaymentsStakeHolders.get(1);
 
-						String amountToCableOperator = splitsPaymentsStakeHolders.get(0);
-						String amountToPyAdvertising = splitsPaymentsStakeHolders.get(1);
+							String uniqueBatchIdperMonth = spotId;
+							String emailSubject = "PyAdvertising Invoice Payment for Order Number " + spotId;
 
-						String uniqueBatchIdperMonth = spotId;
-						String emailSubject = "PyAdvertising Invoice Payment for Order Number " + spotId;
+							String reciepient_type;
+							if (cableOperatorPaypalInfo.contains("@")) {
+								reciepient_type = "EMAIL";
 
-						String reciepient_type;
-						if (cableOperatorPaypalInfo.contains("@")) {
-							reciepient_type = "EMAIL";
+							} else {
+								reciepient_type = "PHONE";
+							}
 
-						} else {
-							reciepient_type = "PHONE";
+							PayOutApplication apps = new PayOutApplication();
+
+							List<PayoutStakeHolderInfo> stakeHoldersInfo = new ArrayList<>();
+							PayoutStakeHolderInfo stakeHolder = new PayoutStakeHolderInfo();
+							stakeHolder.setPaymentNote("This is a payment for Order number " + spotId);
+							Amount amount = new Amount();
+							amount.setCurrency("USD");
+							amount.setValue(amountToCableOperator);
+
+							stakeHolder.setPayoutAmount(amount);
+							stakeHolder.setSenderItemId(spotId);
+							stakeHolder.setRecipeintType(reciepient_type);
+							stakeHolder.setPayPalAccountEmail(cableOperatorPaypalInfo);
+
+							PayoutStakeHolderInfo stakeHolder2 = new PayoutStakeHolderInfo();
+							stakeHolder2.setPaymentNote("This is a comission for Order number " + spotId);
+							Amount amount2 = new Amount();
+							amount2.setCurrency("USD");
+							amount2.setValue(amountToPyAdvertising);
+
+							stakeHolder2.setPayoutAmount(amount2);
+							stakeHolder2.setSenderItemId(spotId);
+							stakeHolder2.setRecipeintType("EMAIL");
+							stakeHolder2.setPayPalAccountEmail(pyAdvertisingPaypalInfo);
+
+							stakeHoldersInfo.add(stakeHolder);
+							stakeHoldersInfo.add(stakeHolder2);
+
+							// Create request
+							PaypalPayoutRequest request = apps.createPaypalPayoutRequest(uniqueBatchIdperMonth,
+									emailSubject, stakeHoldersInfo);
+
+							PrettyPrinterJson.printObject(request);
+							// generate response sends amount to multiple user.
+							PaypalPayoutResponse response = apps.payOut(request);
+
+							PrettyPrinterJson.printObject(response);
+
+							if (response.getError() == null) {
+								paymentStatus = "Payment was successfully completed. No action is needed";
+								
+								boolean isPaymentSavedToDB=savePaymentConfirmationToDB(spotId);
+							} else {
+								paymentStatus = "Something went wrong. Please, contact PyAdvertising. No balance transfered";
+							}
+							
 						}
-
-						PayOutApplication apps = new PayOutApplication();
-
-						List<PayoutStakeHolderInfo> stakeHoldersInfo = new ArrayList<>();
-						PayoutStakeHolderInfo stakeHolder = new PayoutStakeHolderInfo();
-						stakeHolder.setPaymentNote("This is a payment for Order number " + spotId);
-						Amount amount = new Amount();
-						amount.setCurrency("USD");
-						amount.setValue(amountToCableOperator);
-
-						stakeHolder.setPayoutAmount(amount);
-						stakeHolder.setSenderItemId(spotId);
-						stakeHolder.setRecipeintType(reciepient_type);
-						stakeHolder.setPayPalAccountEmail(cableOperatorPaypalInfo);
-
-						PayoutStakeHolderInfo stakeHolder2 = new PayoutStakeHolderInfo();
-						stakeHolder2.setPaymentNote("This is a comission for Order number" + spotId);
-						Amount amount2 = new Amount();
-						amount2.setCurrency("USD");
-						amount2.setValue(amountToPyAdvertising);
-
-						stakeHolder2.setPayoutAmount(amount2);
-						stakeHolder2.setSenderItemId(spotId);
-						stakeHolder2.setRecipeintType("EMAIL");
-						stakeHolder2.setPayPalAccountEmail(pyAdvertisingPaypalInfo);
-
-						stakeHoldersInfo.add(stakeHolder);
-						stakeHoldersInfo.add(stakeHolder2);
-
-						// Create request
-						PaypalPayoutRequest request = apps.createPaypalPayoutRequest(uniqueBatchIdperMonth,
-								emailSubject, stakeHoldersInfo);
-
-						PrettyPrinterJson.printObject(request);
-						// generate response sends amount to multiple user.
-						PaypalPayoutResponse response = apps.payOut(request);
-
-						PrettyPrinterJson.printObject(response);
-
-						if (response.getError() == null) {
-							paymentStatus = "Payment was successfully completed. No action is needed";
-						} else {
-							paymentStatus = "Something went wrong. Please, contact PyAdvertising. No balance transfered";
-						}
-
-					}
-
+						
+					}	
 				} catch (Exception e) {
 					logger.debug(e);
 
@@ -151,10 +173,38 @@ public class OrderConfirmationAndPaymentController {
 				}
 
 			}
+			}
+			else{
+				paymentStatus="The payment to this order id is already completed.";
+			}
 		}
 
 		return paymentStatus;
 
+	}
+
+	/**
+	 * @param spotId
+	 * @return
+	 */
+	private boolean savePaymentConfirmationToDB(String spotId) {
+		// TODO Auto-generated method stub
+		Boolean isPaymentSaved=false;
+		try{
+		String url=ConfigurationHandler.getValueToConfigurationKey("server.url", "env.properties");
+		String resourcePath="/RoboAdPlacer-ApiServices/insertpayment/order/{spotId}";
+		String endPoint=url+resourcePath;
+	
+		Map<String, String> uriParams = new HashMap<String, String>();
+		uriParams.put("spotId", spotId);
+		RestTemplate template= new RestTemplate();
+		isPaymentSaved = template.getForObject(endPoint, Boolean.class, uriParams);
+		logger.info("Payment for OrderId "+spotId+ " is saved. Is payment saved (must Be true) ? "+isPaymentSaved);
+		}
+		catch(HttpClientErrorException e){
+			logger.error(e);
+		}
+		return isPaymentSaved;
 	}
 
 	private static List<String> generateSplitAmountToStakeHolders(GetContractInfoBySpotIdResponse response) {
@@ -198,8 +248,17 @@ public class OrderConfirmationAndPaymentController {
 	}
 
 	public static void main(String[] args) {
+		//This payouts by spotIDs
+		//63=   NjM=
+		String spotId="62";
+		  byte[] encoded = Base64.encodeBase64(spotId.getBytes());   
+		  
+		  String encryptedSpotId=new String(encoded);
+	      
 		OrderConfirmationAndPaymentController controller = new OrderConfirmationAndPaymentController();
-		controller.orderConfirmationAndPayouts("49");
+		controller.orderConfirmationAndPayouts(encryptedSpotId);
+		
+		
 	}
 
 }
